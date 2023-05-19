e182c8c96aa9c4d1719022978eec2347f28a1545
Gateg79 <edikan.fred@gmail.com>
2023-05-19 05:15:42 +0100

    Making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
+    """
+    if total <= 0:
+        return 0
+    cal = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for b in range(coin, total + 1):
+            cal[b] = min(cal[b], cal[b - coin] + 1)
+    # Return: fewest number of coins needed to meet total
+    if cal[-1] != float("inf"):
+        return cal[-1]
+    else:
+        -1
