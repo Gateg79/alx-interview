19c13391a87589d193b5f7b9aaf88b4eaf99c4c1
Gateg79 <edikan.fred@gmail.com>
2023-08-24 15:01:11 +0100

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,18 @@ Change comes from within
 def makeChange(coins, total):
     """
     Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet a given amount total
+    determine the fewest number of coins needed 
+    to meet a given amount total.
     """
     if total <= 0:
         return 0
+    # The total no. of coins     
     cal = [0] + [float("inf")] * (total)
+    
     for coin in coins:
         for b in range(coin, total + 1):
             cal[b] = min(cal[b], cal[b - coin] + 1)
     # Return: fewest number of coins needed to meet total
-    return cal[-1] if cal[-1] != float("inf") else -1
+    if cal[-1] != float("inf"):
+        return cal[-1]
+    return -1
