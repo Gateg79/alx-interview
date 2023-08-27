7c06bed866307398fa176891a7f0c1fb664a276a
Gateg79 <edikan.fred@gmail.com>
2023-08-27 16:03:30 +0100

    Optimal runtime makingChange

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-Change comes from within
-"""
+''' Fewest number of coins needed to meet a given amount
+from a given pile of coins of different values
+'''
+
+import math
+# create makechange function
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values,
-    determine the fewest number of coins needed
-    to meet a given amount total.
-    """
-    if total <= 0:
-        return 0
-    # The total no. of coins
-    cal = [0] + [float("inf")] * (total)
+    cal = [math.inf] * (total+1)
+    cal[0] = 0
+
     for coin in coins:
-        for b in range(coin, total + 1):
-            cal[b] = min(cal[b], cal[b - coin] + 1)
-    # Return: fewest number of coins needed to meet total
-    if cal[-1] != float("inf"):
-        return cal[-1]
-    return -1
+        for b in range(coin, total+1):
+            if b-coin >= 0:
+                cal[b] = min(cal[b], cal[b-coin]+1)
+
+    return -1 if cal[-1] == math.inf else cal[-1]
