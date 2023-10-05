b99868cc31c5e1e958995a6f6f60366b6ae8a882
Gateg79 <edikan.fred@gmail.com>
2023-10-05 22:55:53 +0100

    Prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python3
-"""
-Choosing prime numbers from a list of numbers
-"""
-
-
-def primeNumbers(n):
-    """Return list of prime numbers between 1 and n inclusive
-       Args:
-        n (int): upper boundary of range. lower boundary is always 1
-    """
-    prime = []
-    selectd = [True] * (n + 1)
-    for primeNo in range(2, n + 1):
-        if (selectd[prime]):
-            prime.append(prime)
-            for a in range(primeNo, n + 1, primeNo):
-                selectd[a] = False
-    return prime
+""" Prime game with x rounds and n numbers """
 
 
 def isWinner(x, nums):
     """
-    Determines winner of Prime Game
+    Prime game with x rounds and n numbers
     Args:
-        x (int): no. of rounds of game
-        nums (int): upper limit of range for each round
+        x - number of rounds
+        nums - an array of n, n is a set of
+               consecutive integers starting
+               from 1 up to and incuding n.
     Return:
-        Name of winner (Maria or Ben) or None if winner cannot be found
+        Winner of game
     """
-    if x is None or nums is None or x == 0 or nums == []:
+    if (x == 0 or len(nums) == 0):
         return None
-    Maria = Ben = 0
-    for i in range(x):
-        primeNos = primeNumbers(nums[i])
-        if len(primeNos) % 2 == 0:
-            Ben += 1
+    # Get max n for nums and populate the is_prime array
+    n = max(nums)
+    # Iteration stops at n + 1 so that n is included
+    is_prime = [True for _ in range(n + 1)]
+    # Setting index 0 and 1 to not prime, as it is a known
+    # fact
+    is_prime[0] = False
+    is_prime[1] = False
+
+    # Populating the is_prime array
+    # Iteration must stop at n + 1
+    # so that n is included
+    stop_iteration = n + 1
+    for i in range(2, stop_iteration):
+        if i * i > n:
+            break
+        for j in range(2, stop_iteration):
+            if (i * j) > n:
+                break
+            is_prime[i * j] = False
+
+    # Determining the winner in every round
+    maria_wins = 0
+    ben_wins = 0
+    for round in range(x):
+        is_prime_count = is_prime[1: nums[round] + 1].count(True)
+        if (is_prime_count % 2 == 0 or is_prime_count == 0):
+            ben_wins += 1
         else:
-            Maria += 1
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
-    return None
+            maria_wins += 1
+
+    if maria_wins == ben_wins:
+        return None
+    if maria_wins > ben_wins:
+        return "Maria"
+    return "Ben"
